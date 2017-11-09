b2ab430d9880ce079961d4f9300897813d1f9b67
xander1998 <xander.harrison77@gmail.com>
2017-11-09 03:42:38 -0600

    Added Random Searching Update
    The dog can now search vehicles. [ NOT SURE IF IT CAN SEARCH POLICE
    VEHICLES (ITS LATE SO I PROBABLY MESSED UP ON SOMETHING) ]

--- __resource.lua
+++ __resource.lua
@@ -5,8 +5,10 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 --[[ CONFIG ]]--
-client_script 'cl_k9_config.lua'
+server_script 'sv_k9_config.lua'
 client_script '@warmenu/warmenu.lua'
 --]]
 
+server_script 'version.lua'
+server_script 'sv_k9_handler.lua'
 client_script 'cl_k9.lua'
--- cl_k9.lua
+++ cl_k9.lua
@@ -1,16 +1,26 @@
+--[[
+
+	DO NOT TOUCH THIS. THIS CONTROLS THE DOG IF THIS BREAKS THE DOG BREAKS!!
+
+--]]
+
 local current_dog = nil
 local following = false
 local attacking = false
 local animation_played = nil
 local dog_name = "Max"
 
+local closest_vehicle = nil
+local closest_door_name = nil
+local closest_door_pos = nil
+
 local other_ped_attacked = nil
 
 RegisterNetEvent("K9:ToggleDog")
-AddEventHandler("K9:ToggleDog", function()
+AddEventHandler("K9:ToggleDog", function(data)
 	if current_dog == nil then
 		-- Spawn Dog
-		local model = GetHashKey(K9_Config.DogModel)
+		local model = GetHashKey(data.dogmodel)
 		RequestModel(model)
 		while not HasModelLoaded(model) do
 			RequestModel(model)
@@ -29,20 +39,20 @@ AddEventHandler("K9:ToggleDog", function()
 		BeginTextCommandSetBlipName("STRING");
 		AddTextComponentString(tostring("K9"))
 		EndTextCommandSetBlipName(blip)
-		if K9_Config.GodmodeDog == true then
+		if data.godmode == true then
 			SetEntityInvincible(spawned_entity, true)
 		end
-		K9_Config.Notification("Your dog " .. dog_name .. " is in service.")
+		Notification("Your dog " .. dog_name .. " is in service.")
 	else
 		SetEntityAsMissionEntity(current_dog, true, true)
 		DeleteEntity(current_dog)
-		K9_Config.Notification("Your dog " .. dog_name .. " is out of service.")
+		Notification("Your dog " .. dog_name .. " is out of service.")
 		current_dog = nil
 	end
 end)
 
 RegisterNetEvent("K9:Vehicle")
-AddEventHandler("K9:Vehicle", function()
+AddEventHandler("K9:Vehicle", function(data)
 	if current_dog ~= nil then
 		if IsPedInAnyVehicle(current_dog, false) then
 
@@ -52,9 +62,9 @@ AddEventHandler("K9:Vehicle", function()
 			local plyPos = GetEntityCoords(GetPlayerPed(-1), false)
 			local vehicle = GetClosestVehicle(plyPos['x'], plyPos['y'], plyPos['z'], 3.0, 0, 23)
 
-			if K9_Config.VehicleRestricted == true then
+			if data.VehicleRestricted == true then
 
-				if CheckVehicleRestriction(vehicle) == true then
+				if CheckVehicleRestriction(vehicle, data.VehicleList) == true then
 					TaskEnterVehicle(current_dog, vehicle, -1, 2, 2.0, 1, 0)
 					following = false
 					attacking = false
@@ -78,11 +88,11 @@ AddEventHandler("K9:Follow", function()
 		if following == false then
 			TaskFollowToOffsetOfEntity(current_dog, GetPlayerPed(PlayerId()), 0.5, 0.0, 0.0, 5.0, -1, 0.0, 1)
 			following = true
-			K9_Config.Notification(tostring(dog_name .. " Hier!")) -- Come
+			Notification(tostring(dog_name .. " Hier!")) -- Come
 		else
 			ClearPedTasks(current_dog)
 			following = false
-			K9_Config.Notification(tostring(dog_name .. " Bleib!")) -- Stay
+			Notification(tostring(dog_name .. " Bleib!")) -- Stay
 		end
 	end
 end)
@@ -101,14 +111,14 @@ AddEventHandler("K9:Attack", function()
 				other_ped_attacked = other_ped
 				attacking = true
 				following = false
-				K9_Config.Notification(tostring(dog_name .. " Fass!")) -- Attack
+				Notification(tostring(dog_name .. " Fass!")) -- Attack
 			end
 		end
 	else
 		attacking = false
 		other_ped_attacked = nil
 		ClearPedTasks(dog_ped)
-		K9_Config.Notification(tostring(dog_name .. " Zei Brav!")) -- Stopping Dog (Good Dog)
+		Notification(tostring(dog_name .. " Zei Brav!")) -- Stopping Dog (Good Dog)
 	end
 end)
 
@@ -156,7 +166,7 @@ function sit(entity)
 			ClearPedSecondaryTask(current_dog)
 		end
 		animation_played = nil
-		K9_Config.Notification(tostring(dog_name .. " Stehen!"))
+		Notification(tostring(dog_name .. " Stehen!"))
 	else
 		RequestAnimDict(animdicstart)
 		while not HasAnimDictLoaded(animdicstart) do
@@ -165,7 +175,7 @@ function sit(entity)
 		end
 		TaskPlayAnim(current_dog, animdicstart, animnamestart, 1.0, -1, -1, 2, 0, 0, 0, 0)
 		animation_played = "sit"
-		K9_Config.Notification(tostring(dog_name .. " Sitz!"))
+		Notification(tostring(dog_name .. " Sitz!"))
 	end
 end
 
@@ -186,7 +196,7 @@ function laydown(entity)
 			ClearPedSecondaryTask(current_dog)
 		end
 		animation_played = nil
-		K9_Config.Notification(tostring(dog_name .. " Stehen!"))
+		Notification(tostring(dog_name .. " Stehen!"))
 	else
 		RequestAnimDict(animdicstart)
 		while not HasAnimDictLoaded(animdicstart) do
@@ -195,9 +205,80 @@ function laydown(entity)
 		end
 		TaskPlayAnim(current_dog, animdicstart, animnamestart, 1.0, -1, -1, 2, 0, 0, 0, 0)
 		animation_played = "laydown"
-		K9_Config.Notification(tostring(dog_name .. " Platz!"))
+		Notification(tostring(dog_name .. " Platz!"))
 	end
 
+end
+--]]
+
+--[[ SEARCH FUNCTIONS ]]--
+RegisterNetEvent('K9:Search')
+AddEventHandler('K9:Search', function(data)
+	if not IsPedInAnyVehicle(GetPlayerPed(PlayerId(), false)) then
+		if closest_door_name ~= "Can't Search" then
+			if data.config == "random" then
+				Notification("Dog is searching")
+				local vHeading = GetEntityHeading(closest_vehicle)
+				Citizen.Wait(1500)
+				
+				if closest_door_name == "Front Left Door" then
+					TaskGoToCoordAnyMeans(current_dog, closest_door_pos['x'], closest_door_pos['y'], closest_door_pos['z'], 5.0, 0, 0, 786603, 0xbf800000)
+					Citizen.Wait(3000)
+					TaskAchieveHeading(current_dog, vHeading - 90, -1)
+					SetVehicleDoorOpen(closest_vehicle, 0, false, false)
+				elseif closest_door_name == "Front Right Door" then
+					TaskGoToCoordAnyMeans(current_dog, closest_door_pos['x'], closest_door_pos['y'], closest_door_pos['z'], 5.0, 0, 0, 786603, 0xbf800000)
+					Citizen.Wait(3000)
+					TaskAchieveHeading(current_dog, vHeading - 270, -1)
+					SetVehicleDoorOpen(closest_vehicle, 1, false, false)
+				elseif closest_door_name == "Back Left Door" then
+					TaskGoToCoordAnyMeans(current_dog, closest_door_pos['x'], closest_door_pos['y'], closest_door_pos['z'], 5.0, 0, 0, 786603, 0xbf800000)
+					Citizen.Wait(3000)
+					TaskAchieveHeading(current_dog, vHeading - 90, -1)
+					SetVehicleDoorOpen(closest_vehicle, 2, false, false)
+				elseif closest_door_name == "Back Right Door" then
+					TaskGoToCoordAnyMeans(current_dog, closest_door_pos['x'], closest_door_pos['y'], closest_door_pos['z'], 5.0, 0, 0, 786603, 0xbf800000)
+					Citizen.Wait(3000)
+					TaskAchieveHeading(current_dog, vHeading - 270, -1)
+					SetVehicleDoorOpen(closest_vehicle, 3, false, false)
+				end
+
+				Citizen.Wait(4000)
+				TriggerEvent("chatMessage", tostring("^1K9 Found: ^0" .. RandomSearch(data.items) .. " in the " .. closest_door_name))
+				SetVehicleDoorsShut(closest_vehicle, false)
+			end
+		end
+	end
+end)
+
+function RandomSearch(items)
+	local timesToRun = math.random(1, 4)
+
+	for i = 1, timesToRun do
+		local category = math.random(1, 100)
+
+		if category > 31 and category <=80 then -- Clean Vehicle
+			itemFound = "Nothing Found"
+			return itemFound
+		else
+			if category >= 1 and category <= 10 then -- Illegal Choice
+				local list = items['illegal']
+				local chooseItem = math.random(1, #items['illegal'])
+				itemFound = items['illegal'][chooseItem]
+				return itemFound
+			elseif category > 10 and category <=30 then -- Legal Choice
+				local list = items['legal']
+				local chooseItem = math.random(1, #items['legal'])
+				itemFound = items['legal'][chooseItem]
+				return itemFound
+			elseif category > 80 and category <= 100 then -- Suspicious Items
+				local list = items['suspicious']
+				local chooseItem = math.random(1, #items['suspicious'])
+				itemFound = items['suspicious'][chooseItem]
+				return itemFound
+			end
+		end
+	end
 end
 --]]
 
@@ -207,19 +288,19 @@ Citizen.CreateThread(function()
 		Citizen.Wait(0)
 
 		-- Follow Key --
-		if IsControlJustPressed(1, 47) and not IsPlayerFreeAiming(PlayerId()) then
+		if IsControlJustPressed(1, 47) and not IsPlayerFreeAiming(PlayerId())  and current_dog ~= nil then
 			TriggerEvent("K9:Follow")
 		end
 
 		-- Attacking Key --
-		if IsPlayerFreeAiming(PlayerId()) and IsControlJustPressed(1, 47) then
+		if IsPlayerFreeAiming(PlayerId()) and IsControlJustPressed(1, 47) and current_dog ~= nil then
 			TriggerEvent("K9:Attack")
 		end
 
 		-- If other ped attacking dies it stops attacking --
 		if attacking == true then
 			if IsPedDeadOrDying(other_ped_attacked, 1) then
-				K9_Config.Notification("Pust!") -- Release Bite
+				Notification("Pust!") -- Release Bite
 				TriggerEvent("K9:Attack")
 			end
 		end
@@ -232,18 +313,18 @@ Citizen.CreateThread(function()
 	WarMenu.CreateSubMenu("k9anims", "k9menu")
 	WarMenu.SetTitleBackgroundColor('k9menu', 2, 120, 217, 1.0)
 	WarMenu.SetTitleBackgroundColor('k9anims', 2, 120, 217, 1.0)
-	WarMenu.debug = true
+	WarMenu.debug = false
 
 	while true do
 		-- K9 MENU --
 	  if WarMenu.IsMenuOpened('k9menu') then
 
 			if WarMenu.Button('K9 Toggle [Spawn | Delete]') then
-		  		TriggerEvent("K9:ToggleDog")
+		  		TriggerServerEvent('K9:SendSpawnSettings')
 		   elseif WarMenu.Button("K9 Vehicle [Enter | Exit]") then
-		  		TriggerEvent("K9:Vehicle")
-		   elseif WarMenu.Button("K9 Search") then
-		  		K9_Config.Notification("Not Implemented Yet.")
+		  		TriggerServerEvent("K9:SendVehicleSettings")
+		   elseif WarMenu.Button(tostring("K9 Search: " .. closest_door_name)) then
+				TriggerServerEvent("K9:SendSearchSettings")
 		  	elseif WarMenu.MenuButton("K9 Animations", "k9anims") then
 
 		   elseif WarMenu.Button("Close Menu") then
@@ -266,48 +347,90 @@ Citizen.CreateThread(function()
 
 	  -- Key Toggling
 	  if IsControlPressed(1, 19) and IsControlJustPressed(1,20) then -- LEFTALT + Z
-
-	  		if K9_Config.PedRestricted == true then
-
-	  			if CheckPedRestriction(GetPlayerPed(-1)) == true then
-
-	  				TriggerEvent("K9:OpenMenu")
-
-	  			else
-	  				K9_Config.Notification("You are not an LEO.")
-	  			end
-
-	  		else
-	  			TriggerEvent("K9:OpenMenu")
-	  		end
-
+	  		TriggerServerEvent('K9:SendMenuToggle')
 	  end
 
 	  Citizen.Wait(0)
 	end
 end)
 
+-- Converted to check server config file.
 RegisterNetEvent("K9:OpenMenu")
-AddEventHandler("K9:OpenMenu", function()
-	WarMenu.OpenMenu('k9menu')
+AddEventHandler("K9:OpenMenu", function(data)
+	if data.isRestricted == true then
+		if CheckPedRestriction(GetPlayerPed(-1), data.PedList) == true then
+			GetClosestVehicleDoor()
+			WarMenu.OpenMenu('k9menu')
+		end
+	else
+		WarMenu.OpenMenu('k9menu')
+	end
 end)
 
 --[[ EXTRA FUNCTIONS ]]--
-function CheckVehicleRestriction(vehicle)
-	for i = 1, #K9_Config.VehicleList do
-		if K9_Config.VehicleList[i] == GetEntityModel(vehicle) then
+
+-- Converted to check tables from argument
+function CheckVehicleRestriction(vehicle, VehicleList)
+	for i = 1, #VehicleList do
+		if GetHashKey(VehicleList[i]) == GetEntityModel(vehicle) then
 			return true
 		end
 	end
 	return false
 end
 
-function CheckPedRestriction(ped)
-	for i = 1, #K9_Config.PedList do
-		if K9_Config.PedList[i] == GetEntityModel(ped) then
+-- Converted to check tables from argument
+function CheckPedRestriction(ped, PedList)
+	for i = 1, #PedList do
+		print(tostring(GetHashKey(PedList[i])))
+		if GetHashKey(PedList[i]) == GetEntityModel(ped) then
 			return true
 		end
 	end
 	return false
+end
+
+function GetClosestVehicleDoor()
+	local plyCoords = GetEntityCoords(GetPlayerPed(PlayerId(), false))
+	local vehicle = GetClosestVehicle(plyCoords.x, plyCoords.y, plyCoords.z, 3.0, 0, 70)
+
+	local frontleft = GetWorldPositionOfEntityBone(vehicle, GetEntityBoneIndexByName(vehicle, "door_dside_f"))
+	local frontright = GetWorldPositionOfEntityBone(vehicle, GetEntityBoneIndexByName(vehicle, "door_pside_f"))
+	local backleft = GetWorldPositionOfEntityBone(vehicle, GetEntityBoneIndexByName(vehicle, "door_dside_r"))
+	local backright = GetWorldPositionOfEntityBone(vehicle, GetEntityBoneIndexByName(vehicle, "door_pside_r"))
+
+	local fldistance = GetDistanceBetweenCoords(frontleft['x'], frontleft['y'], frontleft['z'], plyCoords.x, plyCoords.y, plyCoords.z, 1)
+	local frdistance = GetDistanceBetweenCoords(frontright['x'], frontright['y'], frontright['z'], plyCoords.x, plyCoords.y, plyCoords.z, 1)
+	local bldistance = GetDistanceBetweenCoords(backleft['x'], backleft['y'], backleft['z'], plyCoords.x, plyCoords.y, plyCoords.z, 1)
+	local brdistance = GetDistanceBetweenCoords(backright['x'], backright['y'], backright['z'], plyCoords.x, plyCoords.y, plyCoords.z, 1)
+
+	if vehicle ~= 0 then
+		closest_vehicle = vehicle
+		if (fldistance < frdistance and fldistance < bldistance and fldistance < brdistance) then
+			-- Front Left
+			closest_door_pos = frontleft
+			closest_door_name = "Front Left Door"
+		elseif (frdistance < fldistance and frdistance < bldistance and frdistance < brdistance) then
+			-- Front Right
+			closest_door_pos = frontright
+			closest_door_name = "Front Right Door"
+		elseif (bldistance < fldistance and bldistance < frdistance and bldistance < brdistance) then
+			-- Back Left
+			closest_door_pos = backleft
+			closest_door_name = "Back Left Door"
+		elseif(brdistance < fldistance and brdistance < frdistance and brdistance < bldistance) then
+			-- Back Right
+			closest_door_pos = backright
+			closest_door_name = "Back Right Door"
+		end
+	else
+		closest_door_name = "Can't Search"
+	end
+end
+
+function Notification(message)
+	SetNotificationTextEntry("STRING")
+	AddTextComponentString(message)
+	DrawNotification(0, 1)
 end
 --]]
